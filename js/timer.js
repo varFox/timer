bfe3f39fffcf00218b5ab4dfaebe23f0968130e4
Lyubov <aira_dark@live.ru>
2019-04-29 19:24:25 +0300

    2

--- js/timer.js
+++ js/timer.js
@@ -1,3 +1,28 @@
+// 'use strict';
+
+// function zero(a) {
+//   if (a < 10) {
+//     a = '0' + a;
+//   }
+//   return a;
+// }
+
+// function time(h, m, s) {
+//   let hours = h,
+//       minutes = m,
+//       seconds = s;     
+//   let t = setInterval(function() {
+//     document.querySelector('body').textContent = zero(hours) + ':' + zero(minutes) + ':' + zero(seconds);
+//     (seconds > 0) ? seconds -=1 : (seconds == 0 && minutes > 0) 
+//                   ? (seconds = 59, minutes -= 1) : (seconds == 0 && minutes == 0 && hours > 0) 
+//                   ? (seconds = 59, minutes = 59, hours -= 1) 
+//                   : (clearInterval(t), document.querySelector('body').textContent = 'Время вышло');
+//   }, 1000);
+// }
+
+// time(10, 0, 0);
+
+
 'use strict';
 
 function zero(a) {
@@ -7,17 +32,8 @@ function zero(a) {
   return a;
 }
 
-function time(h, m, s) {
-  let hours = h,
-      minutes = m,
-      seconds = s;     
-  let t = setInterval(function() {
-    document.querySelector('body').textContent = zero(hours) + ':' + zero(minutes) + ':' + zero(seconds);
-    (seconds > 0) ? seconds -=1 : (seconds == 0 && minutes > 0) 
-                  ? (seconds = 59, minutes -= 1) : (seconds == 0 && minutes == 0 && hours > 0) 
-                  ? (seconds = 59, minutes = 59, hours -= 1) 
-                  : (clearInterval(t), document.querySelector('body').textContent = 'Время вышло');
-  }, 1000);
-}
-
-time(10, 0, 0);
+setInterval(function() {
+  let now = new Date();
+  document.querySelector('body').textContent = zero(now.getHours()) + ':' + zero(now.getMinutes()) + 
+        ':' + zero(now.getSeconds());
+}, 1000);
